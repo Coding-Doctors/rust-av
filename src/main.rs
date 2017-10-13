906ea4faef3d4d993bdfbf19128056230e9f3ccf
too-r <william@huddie.net>
2017-10-13 20:02:40 +0000

    Add Handler struct and begin writing ban log code

--- src/main.rs
+++ src/main.rs
@@ -7,12 +7,50 @@ extern crate toml;
 use std::path::Path;
 use std::fs::File;
 use std::io::prelude::*;
+use serenity::prelude::*;
+use serenity::model::*;
+use serenity::client::CACHE;
+use std::env;
+use std::collections::HashMap;
+use spin::Mutex;
 
 #[derive(Deserialize)]
 struct Config {
     token: String,
     user_roles: Vec<String>,
 }
+
+static CONFIG: Mutex<Config> = Mutex::new(Config);
+
+struct Handler;
+
+impl EventHandler for Handler {
+    fn on_ready(&self, _: Context, ready: Ready) {
+        println!("{} is connected and serving {} servers.", ready.user.name, ready.guilds.len());
+    }
+
+    fn on_guild_ban_addition(&self, _: Context, guild_id: GuildId, user: User) {
+        let cache = CACHE.read().unwrap();
+
+        if let Some(g) = cache.guild(guild_id) {
+            //Grab a Guild object to play with.
+            let guild = g.read().unwrap();
+
+            let log_channel = {
+                let channels = guild.channels;
+
+                for value in channels.values() {
+                    let mut name = value.read().unwrap().name;
+
+                    match name.as_str() {
+                        s
+                    }
+                }
+            }
+        }
+    }
+}
+
 
 fn main() {
     
