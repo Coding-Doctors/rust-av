cadd6336b0c3c2b8d7de5157be7a98288e383a0c
too-r <william@huddie.net>
2017-10-13 18:09:58 +0000

    Use toml instead of serde_json

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,22 @@
 extern crate serenity;
-extern crate serde_json;
+#[macro_use]
 extern crate serde_derive;
+extern crate toml;
+
+use serde_json::error::Category;
+use std::path::Path;
+use std::fs::File;
+
+#[derive(Deserialize)]
+struct Config {
+    token: String,
+    user_roles: Vec<String>,
+}
 
 fn main() {
-    println!("Hello, world!");
+    
+}
+
+pub fn retrieve_config() -> Result<Config, Option<(usize, usize)>> {
+    let mut path: Path = "$HOME/.con"
 }
