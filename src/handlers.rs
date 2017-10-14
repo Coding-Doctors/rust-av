bf132761ce829d7d4a3a6ddf241a0cda929eea13
too-r <william@huddie.net>
2017-10-14 13:45:30 +0000

    Cargo fmt

--- src/handlers.rs
+++ src/handlers.rs
@@ -7,7 +7,11 @@ pub struct Handler;
 
 impl EventHandler for Handler {
     fn on_ready(&self, _: Context, ready: Ready) {
-        println!("{} is connected and serving {} servers.", ready.user.name, ready.guilds.len());
+        println!(
+            "{} is connected and serving {} servers.",
+            ready.user.name,
+            ready.guilds.len()
+        );
     }
 
     fn on_guild_ban_addition(&self, _: Context, guild_id: GuildId, user: User) {
@@ -17,67 +21,83 @@ impl EventHandler for Handler {
             //Grab a Guild object to play with.
             let guild = g.read().unwrap();
 
-            let log_channel = format!("#{}", CONFIG.lock().log_channel);
+            let log_chan = format!("#{}", CONFIG.lock().log_channel);
 
             let channels = &guild.channels;
 
             for value in channels.values() {
                 let name = &value.read().unwrap().name;
 
-                match name {
-                    log_channel => {
-                        let id = &value.read().unwrap().id;
 
-                        let bans = guild.bans().unwrap();
+                if name == log_chan.as_str() {
+                    let id = &value.read().unwrap().id;
 
-                        for ban in bans {
-                            //Check if the current element of the iterator matches, and if it does
-                            //we can access more info about the ban.
-                            if ban.user == user {
-                                match ban.reason {
-                                    Some(r) => {
-                                        let user_discrim = format!("{}{}", user.name, user.discriminator);
-                                        let message = format!("User {} was banned for reason {}", user_discrim, r);
+                    let bans = guild.bans().unwrap();
 
-                                        if let Err(err) = id.say(&message) {
-                                            println!("Error sending message to channel {} for reason {}", id, err);
-                                        }
-                                    },
+                    for ban in bans {
+                        //Check if the current element of the iterator matches, and if it does
+                        //we can access more info about the ban.
+                        if ban.user == user {
+                            match ban.reason {
+                                Some(r) => {
+                                    let user_discrim =
+                                        format!("{}{}", user.name, user.discriminator);
+                                    let message = format!(
+                                        "User {} was banned for reason {}",
+                                        user_discrim,
+                                        r
+                                    );
 
-                                    None => {
-                                        let user_discrim = format!("{}{}", user.name, user.discriminator);
-                                        let message = format!("User {} was banned. No reason given", user_discrim);
-
-                                        if let Err(err) = id.say(&message) {
-                                            println!("Error sending message to channel {} for reason {}", id, err);
-                                        }
-                                    },
+                                    if let Err(err) = id.say(&message) {
+                                        println!(
+                                            "Error sending message to channel {} for reason {}",
+                                            id,
+                                            err
+                                        );
+                                    }
                                 }
-                            }
-                        }
-                    },
 
-                    _ => {
-                        //Tell the server owner that we couldn't find the correct log channel.
-                        let owner_id = guild.owner_id;
+                                None => {
+                                    let user_discrim =
+                                        format!("{}{}", user.name, user.discriminator);
+                                    let message = format!(
+                                        "User {} was banned. No reason given",
+                                        user_discrim
+                                    );
 
-                        for member in guild.members.values() {
-                            //Get a user object to use.
-                            if member.user.read().unwrap().id == owner_id {
-                                let user = member.user.read().unwrap();
-                                
-                                let info_string = format!("This server does not have a log channel as configured, please make sure that the channel exists and this bot has permission to access it");
-
-                                //Try to dm the server owner that this didn't work.
-                                match user.dm(|m| m.content(&info_string)) {
-                                    Ok(_) => {}
-                                    Err(e) => {
-                                        println!("Error sending log info: {}", e);
+                                    if let Err(err) = id.say(&message) {
+                                        println!(
+                                            "Error sending message to channel {} for reason {}",
+                                            id,
+                                            err
+                                        );
                                     }
                                 }
                             }
                         }
-                    },
+                    }
+                } else {
+                    //Tell the server owner that we couldn't find the correct log channel.
+                    let owner_id = guild.owner_id;
+
+                    for member in guild.members.values() {
+                        //Get a user object to use.
+                        if member.user.read().unwrap().id == owner_id {
+                            let user = member.user.read().unwrap();
+
+                            let info_string = format!(
+                                "This server does not have a log channel as configured, please make sure that the channel exists and this bot has permission to access it"
+                            );
+
+                            //Try to dm the server owner that this didn't work.
+                            match user.dm(|m| m.content(&info_string)) {
+                                Ok(_) => {}
+                                Err(e) => {
+                                    println!("Error sending log info: {}", e);
+                                }
+                            }
+                        }
+                    }
                 }
             }
         }
--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,11 @@
-extern crate serenity;
-#[macro_use]
-extern crate serde_derive;
-extern crate serde;
-extern crate toml;
-extern crate spin;
 #[macro_use]
 extern crate lazy_static;
+extern crate serde;
+#[macro_use]
+extern crate serde_derive;
+extern crate serenity;
+extern crate spin;
+extern crate toml;
 
 mod handlers;
 
