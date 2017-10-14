f24c05c1398598e368d6d81c9a653259daf9dae1
too-r <william@huddie.net>
2017-10-14 13:12:04 +0000

    Fix mutability warnings

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,8 @@ extern crate spin;
 #[macro_use]
 extern crate lazy_static;
 
+mod handlers;
+
 use std::path::Path;
 use std::fs::File;
 use std::io::prelude::*;
@@ -16,6 +18,8 @@ use serenity::client::CACHE;
 use std::env;
 use std::collections::HashMap;
 use spin::Mutex;
+use handlers::Handler;
+
 
 #[derive(Deserialize)]
 struct Config {
@@ -26,7 +30,7 @@ struct Config {
 
 lazy_static! {
     static ref CONFIG: Mutex<Config> = {
-        let mut path = "$HOME/.config/sudobot/config.toml";
+        let path = "$HOME/.config/sudobot/config.toml";
         let mut f = File::open(path).unwrap();
 
         let mut buffer = String::new();
@@ -38,89 +42,12 @@ lazy_static! {
     };
 }
 
-
-struct Handler;
-
-impl EventHandler for Handler {
-    fn on_ready(&self, _: Context, ready: Ready) {
-        println!("{} is connected and serving {} servers.", ready.user.name, ready.guilds.len());
-    }
-
-    fn on_guild_ban_addition(&self, _: Context, guild_id: GuildId, user: User) {
-        let cache = CACHE.read().unwrap();
-
-        if let Some(g) = cache.guild(guild_id) {
-            //Grab a Guild object to play with.
-            let mut guild = g.read().unwrap();
-
-            let log_channel = format!("#{}", CONFIG.lock().log_channel);
-
-            let channels = &guild.channels;
-
-            for value in channels.values() {
-                let mut name = &value.read().unwrap().name;
-
-                match name {
-                    log_channel => {
-                        let mut id = &value.read().unwrap().id;
-
-                        let mut bans = guild.bans().unwrap();
-
-                        for ban in bans {
-                            //Check if the current element of the iterator matches, and if it does
-                            //we can access more info about the ban.
-                            if ban.user == user {
-                                match ban.reason {
-                                    Some(r) => {
-                                        let user_discrim = format!("{}{}", user.name, user.discriminator);
-                                        let message = format!("User {} was banned for reason {}", user_discrim, r);
-
-                                        if let Err(err) = id.say(&message) {
-                                            println!("Error sending message to channel {} for reason {}", id, err);
-                                        }
-                                    },
-
-                                    None => {
-                                        let user_discrim = format!("{}{}", user.name, user.discriminator);
-                                        let message = format!("User {} was banned. No reason given", user_discrim);
-
-                                        if let Err(err) = id.say(&message) {
-                                            println!("Error sending message to channel {} for reason {}", id, err);
-                                        }
-                                    },
-                                }
-                            }
-                        }
-                    },
-
-                    _ => {
-                        //Tell the server owner that we couldn't find the correct log channel.
-                        let mut owner_id = guild.owner_id;
-
-                        for member in guild.members.values() {
-                            //Get a user object to use.
-                            if member.user.read().unwrap().id == owner_id {
-                                let user = member.user.read().unwrap();
-                                
-                                let info_string = format!("This server does not have a log channel as configured, please make sure that the channel exists and this bot has permission to access it");
-
-                                //Try to dm the server owner that this didn't work.
-                                match user.dm(|m| m.content(&info_string)) {
-                                    Ok(_) => {}
-                                    Err(e) => {
-                                        println!("Error sending log info: {}", e);
-                                    }
-                                }
-                            }
-                        }
-                    },
-                }
-            }
-        }
-    }
-}
-
-
 fn main() {
-    
+    let token = &CONFIG.lock().token;
+
+    let mut client = Client::new(&token, Handler);
+
+    if let Err(e) = client.start() {
+        println!("Error starting client {}", e);
+    }
 }
