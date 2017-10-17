0afe84c7f9697a57bf7df242cfbab98cc32a002d
too-r <william@huddie.net>
2017-10-17 09:09:35 +0000

    Actually fix guild oject this time (This builds now)

--- src/handlers/moderation.rs
+++ src/handlers/moderation.rs
@@ -8,21 +8,12 @@ pub fn ban_handler(_: Context, guild_id: GuildId, user: User, cfg: Config) -> Re
     //This is safe because we are the only ones who hold the lock.
     let cache = CACHE.read().unwrap();
 
-    let mut guild = cache.guild(guild_id);
-    
-    //No guild found for the specified guild id.
-    if guild.is_none() {
-        Err(format!("No guild found for this guild id."))
-    }
-
-    //Safe.
-    guild = guild.unwrap().read();
+    let cached_guild = cache.guild(guild_id).ok_or_else(|| error!("No guild found for guild id {}", guild_id));
+    let cache_unwrap = cached_guild.unwrap();
+    let guild = cache_unwrap.read().unwrap();
 
     let log_msg: String;
-    let guild_bans = match guild.bans() {
-        Ok(b) => b,
-        Err(e) => Err(&format!("Couldn't pull bans from discord: {}", e))
-    };
+    let guild_bans = guild.bans().unwrap();
 
     //The ban we want to log.
     match guild_bans.iter().find(|b| b.user.id == user.id) {
@@ -34,11 +25,11 @@ pub fn ban_handler(_: Context, guild_id: GuildId, user: User, cfg: Config) -> Re
             } else {
                 log_msg = format!("User {} was banned for reason {}", user.name, reason.clone().unwrap());
             }
-            log_msg
+            Ok(log_msg)
         },
         None => {
             log_msg = format!("User {} is not banned from server", user.name);
-            log_msg
+            Ok(log_msg)
         }
     }
 }
