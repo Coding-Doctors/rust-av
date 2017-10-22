6100f71966482cfe8339cf5a384d20d6031be489
too-r <william@huddie.net>
2017-10-22 16:55:19 +0000

    Back to the old unwrap until I figure out errors

--- src/main.rs
+++ src/main.rs
@@ -25,7 +25,7 @@ use std::env;
 use std::error::Error;
 use spin::Mutex;
 use handlers::Handler;
-use error;
+
 
 #[derive(Deserialize)]
 pub struct Config {
@@ -33,16 +33,16 @@ pub struct Config {
     log_channel: u64,
 }
 
-pub fn get_config() -> Result<Config, error::Error> {
-    let home_dir = env::home_dir()?;
+pub fn get_config() -> Config {
+    let home_dir = env::home_dir().unwrap();
     let path = home_dir.join(".config").join("sudobot").join("config.toml");
 
-    let mut f = File::open(path)?;
+    let mut f = File::open(path).unwrap();
     let mut buf = String::new();
 
-    f.read_to_string(&mut buf)?;
+    f.read_to_string(&mut buf).unwrap();
 
-    toml::from_str(&buf)
+    toml::from_str(&buf).unwrap()
 }
 
 fn main() {
@@ -53,15 +53,9 @@ fn main() {
         },
     }
 
-    let cfg = match get_config() {
-        Ok(c) => c,
-        Err(e) => {
-            //Io error.
-            error!("Error retrieving config from file: {}", e.description());
-        },
-    };
+    let cfg = get_config();
         
-    let token = cfg.clone().token;
+    let token = cfg.token;
     
     let handler = Handler {
         cfg: cfg,
