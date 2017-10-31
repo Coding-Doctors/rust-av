45f934d7723f7ad95ec994b607ee6298dd5342d8
too-r <william@huddie.net>
2017-10-31 15:35:27 +0000

    Reimplement reasons which actually work now

--- src/commands/mod.rs
+++ src/commands/mod.rs
@@ -9,9 +9,21 @@ command!(ban(_ctx, msg, args) {
     let guild = guild.read().unwrap();
 
     let id = msg.mentions[0].id;
+    
+    let mut reason: String;
+
+    if let Some((_, remainder)) = args.split_first() {
+        let joined = remainder.join(" ");
+        reason = joined;
+    } else {
+        msg.channel_id.say("You must provide at least one argument to this command for it to work.\nThe first argument must be a mention of the user you want to ban");
+        reason = String::new();
+    }
+
+    let options: (u8, &str) = (1, &reason);
 
     //Delete messages in the past day. The BanOptions is a tuple implementation for (u8, str).
-    if let Err(why) = guild.ban(id, 1) {
+    if let Err(why) = guild.ban(id, options) {
         error!("Error banning user: {}", why);
     }
 });
